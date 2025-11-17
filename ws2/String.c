851146593a60c707c1833dda0100e8e26ed44e91
rozaline.kozly <rozaline.ko@gmail.com>
2025-11-17 18:46:28 +0200

    Fix: string functions to behave like the libc ones
    StrCmp: fixed the while loop so it actually compares full strings.
    StrDup: removed the extra manual copying and now only uses StrCpy.
    StrStr: fixed the search flow so it continues correctly after mismatch.
    StrnCmp: fixed behavior for n==0 and end-of-string cases. and removed uses of string.h library to enhance modularity.

--- ws2/String.c
+++ ws2/String.c
@@ -4,26 +4,45 @@
 * DATE 16-11-2025
 ********************************************/
 #include <stddef.h> /* using size_t data type */
-#include <string.h> /* used strlen() */
-#include <stdlib.h> /* strdup uses malloc */
-#include <stdio.h> /* seven boom uses printf */
+#include <stdlib.h> /* strdup uses malloc and free */
 #include "String.h" /* my declarations*/
-#include <assert.h> /* for asserts */
+#include <assert.h> /* for asserts (debugging mode) */
 #define SEVEN 7
 
 size_t StrLen(const char* str)
 {
   size_t len = 0;
+  
   assert(NULL != str);
+  
   while('\0' != *str)
   {
     ++len;
+    ++str;
   }
   
   return len;
-
 }
 
+
+int StrCmp(const char *s1, const char *s2)
+{
+  while(*s1 != '\0' && *s2 != '\0')
+  {	
+	if(*s1 == *s2)
+	{
+		++s1;
+		++s2;
+			
+	}
+	else
+	{
+		break;
+	}
+  } 
+	
+  return ((unsigned char)(*s1) - (unsigned char)(*s2));
+}
 
 char* StrCpy(char* dst, const char* src)
 {
@@ -46,177 +65,108 @@ char* StrCpy(char* dst, const char* src)
 
 char* StrnCpy(char* dst, const char* src, size_t n)
 {
-    size_t counter = 0;
     char* head = dst;
-
-    assert(NULL != dst);
-    assert(NULL != src);
-    assert(StrLen(dest) <= n);
     
-    /* copy up to n chars or until \0 */
-    while ((*src != '\0') && (counter < n))
+    assert(dst != NULL && src != NULL);
+
+    while(n > 0 && *src != '\0')
     {
-        *dst = *src;
-        ++src;
-        ++dst;
-        ++counter;
+        *dst++ = *src++;
+        --n;
     }
 
-    /* if src shorter than n then pad with \0 */
-    while (counter < n)
+    while(n > 0) /* padding with null terminator in case |src| < n */
     {
-        *dst = '\0';
-        ++dst;
-        ++counter;
+        *dst++ = '\0';
+        --n;
     }
 
     return head;
 }
 
 
-int StrCmp(const char *str1, const char *str2)
-{
-	int diff = 0;
-	size_t i = 0;
-	
-	assert(NULL !=str1);
-	assert(NULL !=str2);
-	
-	while('\0' != *str1 && '\0' != *str2)
-	{
-		diff = str1[i] - str2[i];
-		if(diff != 0) 
-		{
-			return diff;
-		}
-		
-		++i;
-		
-	}
-	
-	if('\0' == *str1)
-	{
-	  return (*str1);
-	}
-	else
-	{
-	  return (0 - *str2);
-	}
-	
-	return 0;
-}
-
 int StrnCmp(const char *str1, const char *str2, size_t n)
 {
-	int diff = 0;
-	size_t i = 0;
-	
-	assert(NULL !=str1);
-	assert(NULL !=str2);
-	
-	while(n>0)
-	{
-		diff = str1[i] - str2[i];
-		if(diff != 0) 
-		{
-			return diff;
-		}
-		
-		++i;
-		--n;
-	}
-	
-	return 0;
+    assert(str1 != NULL);
+    assert(str2 != NULL);
 
+    while(n > 0 && *str1 == *str2 && *str1 != '\0')
+    {
+        ++str1;
+        ++str2;
+        --n;
+    }
+
+    if(n == 0)
+    {
+        return 0;
+    }
+
+    return (unsigned char)*str1 - (unsigned char)*str2;
 }
+
 
 int StrCaseCmp(const char* s1, const char* s2)
 {
-	int i = 0;
-	int diff = 0;
-	
-	char c1 = ' ';
-	char c2 = ' ';
-	  
- 	while('\0' != s1[i] && '\0' != s2[i])
- 	{
- 		c1 = s1[i];
- 		c2 = s2[i];
- 		if(c1 >= 'A' && c1 <= 'Z')
- 		{
- 			c1 = c1 - 'A' + 'a';
- 		}
- 		if(c2 >= 'A' && c2 <= 'Z')
- 		{
- 			c2 = c2 - 'A' + 'a';
- 		}
- 		
- 		diff = c1 - c2;
- 		if(0 != diff)
- 		{
- 			return diff;
- 		}
- 		++i;
- 	}
- 	
- 	if('\0' != s1[i])
- 	{
- 		return ((int)s1[i]);
- 	}
- 	else if ('\0' != s2[i])
- 	{
- 		return ((int)s2[i]*(-1));
- 	}
- 	
- 	return diff;
-}
+    unsigned char c1, c2;
 
+    while(*s1 && *s2)
+    {
+        c1 = (*s1 >= 'A' && *s1 <= 'Z') ? *s1 - 'A' + 'a' : *s1;
+        c2 = (*s2 >= 'A' && *s2 <= 'Z') ? *s2 - 'A' + 'a' : *s2;
+
+        if(c1 != c2)
+        {
+            return c1 - c2;
+        }
+
+        ++s1;
+        ++s2;
+    }
+
+    c1 = (*s1 >= 'A' && *s1 <= 'Z') ? *s1 - 'A' + 'a' : *s1;
+    c2 = (*s2 >= 'A' && *s2 <= 'Z') ? *s2 - 'A' + 'a' : *s2;
+
+    return c1 - c2;
+}
 
 char* StrChr(const char *str, int ch)
 {
+    char* iterator = (char*)str;
 
-	char* iterator = (char*) str; 
+    while(*iterator != '\0')
+    {
+        if(*iterator == ch)
+        {
+            return iterator;
+        }
+        ++iterator;
+    }
 
-	while('\0' != (*iterator))
-	{
-		if(ch == *iterator)
-		{
-			return iterator;		
-		}
-		iterator++;;		
-	
-	}
-	
-	if('\0' == ch)
-	{
-		return iterator; /* the last char \0 in the given string*/
-	}
-	
-	return NULL; 
+    if(ch == '\0')
+    {
+        return iterator;
+    }
 
+    return NULL;
 }
-
 
 char* StrDup(const char* string)
 {
-	char* duplicate_string = NULL;
-	size_t i  = 0;
-	duplicate_string = (char*)malloc(strlen(string)+1);
-	if(NULL == duplicate_string) 
-	{
-		return NULL;
-	}
-	
-	while('\0' != string[i])
-	{
-		duplicate_string[i] = string[i];
-		i++;	
-	}
-	
-	StrCpy(duplicate_string,string);
-	return duplicate_string;
+    char* duplicate_string = NULL;
+    size_t len = StrLen(string);
 
+    duplicate_string = (char*)malloc(len + 1);
+    if(NULL == duplicate_string)
+    {
+        return NULL;
+    }
+
+    StrCpy(duplicate_string, string);
+
+    return duplicate_string;
 }
+
 
 char* StrCat(char *dest, const char *src)
 {
@@ -267,7 +217,7 @@ char* StrStr(const char *s1, const char *s2)
 {
     char* traverse_s1 = (char*)s1;
     char first_letter_of_s2 = *s2;
-    size_t str2_len = strlen(s2);
+    size_t str2_len = StrLen(s2);
     int strcmp_res = -1;
 
     if ('\0' == *s2)
@@ -327,145 +277,7 @@ size_t StrSpn(const char* str1, const char* str2)
 }
 
 
-/* questions in the worksheet*/
-size_t IsPalindrome(char* str)
-{
-    size_t len = strlen(str);
-    size_t i = 0;
-    size_t j = len - 1;
-
-    if (len <= 1)
-    {
-        return 1;
-    }
-
-
-
-    while (j > i)
-    {
-        if (str[i] != str[j])
-        {
-            return 0;
-        }
-        ++i;
-        --j;
-    }
-
-    return 1;
-}
 
 
 
 
-void SevenBoom (int start, int end)
-{
-	int iterator = start;
-	int remainder = 0;
-	int num = 0;
-	size_t flag = 0;
-	size_t counter_to_new_line = 10;
-	
-	/* invalid range input */
-	if(start > end)
-	{
-		printf(RED "INVALID INPUT!\n");
-	}
-	
-	
-	for( iterator = start ; iterator <= end ; iterator++)
-	{
-		flag = 0;
-		if(counter_to_new_line == 0)
-		{
-			printf("\n");
-			counter_to_new_line = 10;
-		
-		}
-
-		
-		/* check if it is dividable by 7 */
-		if(iterator % SEVEN == 0)
-		{
-			printf("BOOM ");
-			counter_to_new_line--;
-			flag = 1;
-		
-		}
-		/*check if 7 is one of it's digits */
-		else 
-		{
-			num = iterator;
-			while(num != 0)
-			{
-				remainder = num % 10;
-				if(remainder == SEVEN)
-				{
-					printf("BOOM ");
-					counter_to_new_line--;
-					flag = 1;
-					break;
-				}
-				num = num / 10;
-			}
-		
-		}
-		if(flag == 0)
-		{
-			printf("%d ", iterator);
-			counter_to_new_line--;
-		}
-		
-		
-	}
-	
-	printf("\n");
-
-
-}
-
-char* WhiteSpaceReview(char* str)
-{
-    char* read = str; /* ptr that reads */
-    char* write = str; /* ptr that writes*/
-    int in_ws = 0; /* flag to signal if we are in a whitespace sequence or not */
-    char first_ws = 0; /* holds first whitespace in the current sequence (space \ tab)*/
-
-   
-    /* skip spaces and tabs from the begining of the string */
-    while (*read == ' ' || *read == '\t')  
-    {
-        ++read;
-    }
- 
- 
-    /* middle & last part of the string */
-    while (*read != '\0')
-    {
-        if (*read == ' ' || *read == '\t') /* a whitespace found */
-        {
-        	   /* found first whitespace in the sequence */
-            if (!in_ws) 
-            {
-                in_ws = 1;
-                first_ws = *read; /* save first whitespace in the sequence */
-            }
-            ++read; 
-        }
-        else /* a sequence of whitespaces stopped (found a different char)*/
-        {
-            if (in_ws && write != str) /* we skip writing in the beginning 
-                                        (prevents insertions of ws at the start*/
-            {
-                *write = first_ws;
-                ++write;
-            }
-            in_ws = 0;
-            *write = *read;
-            ++write;
-            ++read;
-        }
-    }
-    *write = '\0';
-
-    return str;
-}
