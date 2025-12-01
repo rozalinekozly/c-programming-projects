7202e734dcd9551d5297d8edb7953b75c9456d0a
rozaline.kozly <rozaline.ko@gmail.com>
2025-12-01 15:06:03 +0200

    NEW BRANCH WS9: intial commit for the branch to make it a remote one

---
+++ c/ws9/testing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+     int i; 
+    int sum = 0;
+
+    for (i = 0; i < 1000000; i++) {
+        sum += i;
+    }
+
+    printf("Sum: %d\n", sum);
+    return 0;
+}
