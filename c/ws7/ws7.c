75ad28e7b8afd51cc2272e0298c030a7ff3dabf8
rozaline.kozly <rozaline.ko@gmail.com>
2025-12-01 08:59:13 +0200

    worksheet 7

--- c/ws7/ws7.c
+++ c/ws7/ws7.c
@@ -1,238 +1,199 @@
-/***************************************
+/******************************************
 submitter : rozaline kozly
-reviewer : shir 
-version 2
-worksheet 8 (struct)
-branch ws7 - 
-****************************************/
-#include <stdio.h>          /*  printf          */
-#include <stdlib.h>         /* malloc,   free  */
-#include <string.h>        /* strlen, strcat  */
-#include <assert.h>       /* assert */
-/* magic numbers */
-#define MAX_LEN                   80
+reviewer  : shir 
+version   : clean + fixed
+worksheet : 8 (structs)
+******************************************/
 
-/* macros */
-#define SIZE_OF_ARRAY(ARRAY)      ((sizeof(ARRAY) / (sizeof((ARRAY)[0])))
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* malloc, free, realloc */
+#include <string.h>     /* strlen, strcpy, strcat */
+#include <assert.h>     /* assert */
 
-/* sizeof macros*/
-#define SIZEOF_VAR(X)             ((char *)(&X + 1) - (char *)&X)   
-#define SIZEOF_TYPE(T)            ((size_t)((T*)0 + 1) - (size_t)((T*)0))
-
-/* max macros*/
-#define MAX2(a, b)               (((a) > (b)) ? (a) : (b))
-#define MAX3(a, b, c)            ((((a) > (b) && (a) > (c)) ? (a) : (MAX2((b), (c)))))
-
-void TestMaxMacros();
-void TestElement();
-void TestSizeOfMacros();
+#define MAX_LEN 80
 
 typedef enum
 {
-  TYPE_INT,
-  TYPE_FLOAT,
-  TYPE_STRING
+    TYPE_INT,
+    TYPE_FLOAT,
+    TYPE_STRING
 } ElementType;
 
 typedef struct
 {
-  void* data;
-  void (*add)(void* data, int n);
-  void (*print)(void* data);
-  void (*cleanup)(void* data);
+    void *data;
+
+    void (*add)(void *data, int n);
+    void (*print)(void *data);
+    void (*cleanup)(void *data);
+
 } element_ty;
 
-/*********************************** int type functions   *************************************/
-void IntAdd(void* data, int n)
+/**********************************************
+   INT IMPLEMENTATION
+**********************************************/
+void IntAdd(void *data, int n)
 {
-  *(int*)data += n;
+    *(int *)data += n;
 }
 
-void IntPrint(void* data)
+void IntPrint(void *data)
 {
-  printf("%d", *(int*)data);
+    printf("%d", *(int *)data);
 }
 
-void IntCleanup(void* data)
+void IntCleanup(void *data)
 {
-  free(data);
+    /* fixed-size → just free memory */
+    free(data);
 }
 
-/*********************************** float type functions   *************************************/
-void FloatAdd(void* data, int n)
+/**********************************************
+   FLOAT IMPLEMENTATION
+**********************************************/
+void FloatAdd(void *data, int n)
 {
-  *(float*)data += (float)n;
+    *(float *)data += (float)n;
 }
 
-void FloatPrint(void* data)
+void FloatPrint(void *data)
 {
-  printf("%f", *(float*)data);
+    printf("%f", *(float *)data);
 }
 
-void FloatCleanup(void* data)
+void FloatCleanup(void *data)
 {
-  free(data);
+    free(data);
 }
 
-/*********************************** string type functions   *************************************/
-void StringAdd(void* data, int n)
+/**********************************************
+   STRING IMPLEMENTATION  (realloc allowed)
+**********************************************/
+void StringAdd(void *data, int n)
 {
-  char buf[MAX_LEN] ; /* placement to add the integer as a string */
-  size_t old_len = 0;
-  size_t add_len = 0;
-  char* new_data = NULL;
-  
-   sprintf(buf, "%d", n);
-   old_len = strlen((char*)data);
-   add_len = strlen(buf);
-   new_data = realloc(data, old_len + add_len + 1); /* +1 for \0 */
-   assert(NULL != new_data);
-   data = new_data; /* data has the new size */
-   strcat((char*)data, buf); /* concatenate the strings */
+    char buf[MAX_LEN];
+    size_t old_len = 0;
+    size_t add_len = 0;
+    char *new_data;
+
+    sprintf(buf, "%d", n);
+    old_len = strlen((char *)data);
+    add_len = strlen(buf);
+    new_data = realloc(data, old_len + add_len + 1);
+    assert(new_data != NULL);
+    printf("here");
+    strcat(new_data, buf); 
+    data = new_data; /* data has the new size */
+   strcat(*(char**)data, buf); /* concatenate the strings */
+  /*  *(void **)data = new_data; */
 }
 
-void StringPrint(void* data)
+void StringPrint(void *data)
 {
-  printf("%s", (char*)data);
+    printf("%s", (char *)data);
 }
 
-void StringCleanup(void* data)
+void StringCleanup(void *data)
 {
-  free(data);
+    free(data);
 }
 
-/*********************************** element constructor  *************************************/
-element_ty CreateElement(ElementType type, void* value) /* constructor for an element */
+/**********************************************
+   ELEMENT CONSTRUCTOR
+**********************************************/
+element_ty CreateElement(ElementType type, const void *value)
 {
     element_ty e;
-    e.data = NULL;
 
-    switch(type) /* defines the relevant methods based on the instance's type*/
+    switch (type)
     {
-      case (TYPE_INT):
-      e.data = malloc(sizeof(int));
-      *(int*)e.data = *(const int*)value;
-      e.add = IntAdd;
-      e.print = IntPrint;
-      e.cleanup = IntCleanup;
-      break;
+        case TYPE_INT:
+        {
+            e.data = malloc(sizeof(int));
+            assert(e.data != NULL);
 
-      case (TYPE_FLOAT):
-      e.data = malloc(sizeof(float));
-      *(float*)e.data = *(const float*)value;
-      e.add = FloatAdd;
-      e.print = FloatPrint;
-      e.cleanup = FloatCleanup;
-      break;
+            *(int *)e.data = *(const int *)value;
 
-      case (TYPE_STRING):
-      {
-      const char* src = (const char*)value;
-      e.data = malloc(strlen(src) + 1);
-      strcpy((char*)e.data, src);
-      e.add = StringAdd;
-      e.print = StringPrint;
-      e.cleanup = StringCleanup;
-      break;
-      }
+            e.add = IntAdd;
+            e.print = IntPrint;
+            e.cleanup = IntCleanup;
+
+            break;
+        }
+
+        case TYPE_FLOAT:
+        {
+            e.data = malloc(sizeof(float));
+            assert(e.data != NULL);
+
+            *(float *)e.data = *(const float *)value;
+
+            e.add = FloatAdd;
+            e.print = FloatPrint;
+            e.cleanup = FloatCleanup;
+
+            break;
+        }
+
+        case TYPE_STRING:
+        {
+            const char *src = (const char *)value;
+            e.data = malloc(strlen(src) + 1);
+            assert(e.data != NULL);
+
+            strcpy((char *)e.data, src);
+
+            e.add = StringAdd;
+            e.print = StringPrint;
+            e.cleanup = StringCleanup;
+
+            break;
+        }
     }
 
-return e;
+    return e;
 }
 
+/**********************************************
+   TEST
+**********************************************/
 int main()
 {
-    TestElement();
-    TestMaxMacros();
-    TestSizeOfMacros();
-    
+    int i_val = 3;
+    float f_val = 2.5f;
+
+    element_ty arr[3];
+
+    arr[0] = CreateElement(TYPE_INT, &i_val);
+    arr[1] = CreateElement(TYPE_FLOAT, &f_val);
+    arr[2] = CreateElement(TYPE_STRING, "hi");
+
+    /* print initial */
+    arr[0].print(arr[0].data); printf("\n");
+    arr[1].print(arr[1].data); printf("\n");
+    arr[2].print(arr[2].data); printf("\n");
+
+    /* add 10 */
+    arr[0].add(arr[0].data, 10);
+    arr[1].add(arr[1].data, 10);
+
+    {
+        /* string special case: add needs pointer-to-pointer */
+        void *tmp = arr[2].data;
+        StringAdd(&tmp, 10);
+        arr[2].data = tmp;
+    }
+
+    /* print after */
+    arr[0].print(arr[0].data); printf("\n");
+    arr[1].print(arr[1].data); printf("\n");
+    arr[2].print(arr[2].data); printf("\n");
+
+    /* cleanup */
+    arr[0].cleanup(arr[0].data);
+    arr[1].cleanup(arr[1].data);
+    arr[2].cleanup(arr[2].data);
+
     return 0;
 }
 
-void TestElement()
-{
-    int i_int = 3;
-    float f_float = 2.5;
-    size_t i;
-    
-    element_ty elements[3];
-
-    elements[0] = CreateElement(TYPE_INT, &i_int);
-    elements[1] = CreateElement(TYPE_FLOAT, &f_float);
-    elements[2] = CreateElement(TYPE_STRING, "hi");
-
-   /* printing */
-    for (i = 0; i < 3; i++)
-    {
-        elements[i].print(elements[i].data);
-        printf("\n");
-    }
-
-    /* addition */
-    for (i = 0; i < 3; i++)
-    {
-        elements[i].add(elements[i].data, 10);
-    }
-    
-    /* printing after addition */
-    for (i = 0; i < 3; i++)
-    {
-        elements[i].print(elements[i].data);
-        printf("\n");
-    }
-
-    /* cleanup */
-    for (i = 0; i < 3; i++)
-    {
-        elements[i].cleanup(elements[i].data);
-    }
-    
-}
-void TestMaxMacros()
-{
-    int a = -3;
-    int b = 415;
-    int c = 22;
-    
-    int expected_max_a_b = b;
-    int expected_max_b_c = b;
-    int expected_max_a_c = c;
-    int expected_max_a_b_c = b;
-    
-    MAX2(a, b) == expected_max_a_b ? 1 : printf("FAILED \n");
-    MAX2(b, c) == expected_max_b_c ? 1 : printf("FAILED\n");
-    MAX2(a, c) == expected_max_a_c ? 1 : printf("FAILED\n");
-    MAX3(a, b, c) == expected_max_a_b_c ? 1 : printf("FAILED\n");
-
-
-  return;
-
-}
-
-void TestSizeOfMacros()
-{
-    float f_tmp;
-    size_t s_tmp;
-    int i_tmp;
-    
-    if( SIZEOF_VAR(i_tmp) != sizeof(i_tmp))
-    {
-        printf("FAILED at sizeof int var\n");
-    }
-     if( SIZEOF_VAR(f_tmp) != sizeof(f_tmp))
-    {
-        printf("FAILED at sizeof int var\n");
-    }
-     if( SIZEOF_VAR(s_tmp) != sizeof(s_tmp))
-    {
-        printf("FAILED at sizeof int var\n");
-    }
-     if( SIZEOF_TYPE(int) != sizeof(int))
-    {
-        printf("FAILED at sizeof int var\n");
-    }
-        if( SIZEOF_TYPE(element_ty) != sizeof(element_ty))
-    {
-        printf("FAILED at sizeof int var\n");
-    }
-}
