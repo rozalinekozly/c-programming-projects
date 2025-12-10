b0e875089d4bd059533474fcfada12ed97566590
rozaline.kozly <rozaline.ko@gmail.com>
2025-12-10 14:19:35 +0200

    update: testing file to support 3 type of data (int, char, float), and it performs testing on the stack, it can be more elegant by combining different types of data within the same stack

--- ds/test/stack_test.c
+++ ds/test/stack_test.c
@@ -1,85 +1,252 @@
 /*--------------------------------------------------------------------------
 submitter : Rozaline Kozly
-reviewer  : 
+reviewer  : steve
 worksheet : 12 (ds - stack)
 version   : 1
-date      : 9 Dec 2025
-stage     : pre-review
+date      : 10 Dec 2025
+stage     : post-testing (intial-testing-peerReview-mimir)
 ----------------------------------------------------------------------------*/
 #include "../include/stack.h"
-#include <stdio.h>
-/*
-static void TestStackCreate();
-static void TestStackDestroy();
-static void TestStackPush();
-static void TestStackPeek();
-static void TestStackPop();
-static void TestStackIsEmpty();
-static void TestStackSize();
-static void TestStackCapacity();
-*/
+#include <stdio.h>	/* printf() */
+#include <math.h>		/* pow()   */
+#include <stdlib.h>	/* rand() */
+#include  <limits.h>		/*INT_MAX*/
+
+#define SIZEOF_ARR(ARR)  		(sizeof(ARR)/sizeof(*ARR))
+#define ASCII_SIZE				256
+
+static void Test();
+
+static void TestInt(size_t capacity, size_t element_size, int* elements, size_t number_of_elements);
+static void TestFloat(size_t capacity, size_t element_size, float* elements, size_t number_of_elements);
+static void TestChar(size_t capacity, size_t element_size, char* elements, size_t number_of_elements);
+
+static size_t test_number = 1;
+
 int main()
+{	
+	Test();
+	return 0;	
+}
+
+
+static void Test()
 {
-	stack_ty* stack_p =  StackCreate(10, sizeof(int));
+	size_t capacity = 100;
+	int elements_int[100] = {0};
+	float elements_float[100] = {0};
+	char elements_char[100] = {0};
 	size_t i = 0;
-	int val = 5;
-	int my_val = 4;
-	for(i = 0 ; i < 10 ; i++)
+	
+	
+	/* creating random values of each type */
+	for(i = 0 ; i < capacity ; i++)
 	{
-	    StackPush(stack_p, &my_val);
-	    printf("stack size = %lu\n", StackSize(stack_p));
-	    printf("stack is empty? %d\n",StackIsEmpty(stack_p));
-	    val = *((int*) StackPeek(stack_p));
-	    printf("val is %d\n", val);
-
-	}
+		elements_int[i] = pow(-1,i)*(rand()%INT_MAX );
+		elements_float[i] = pow(-1,i)*((float)(rand()% INT_MAX)/(rand() + 1));
+		elements_char[i] = (rand() % ASCII_SIZE);
 	
+	}
+	/* call test int */
+	TestInt(capacity, sizeof(int), elements_int, 100);
+	TestFloat(capacity, sizeof(float), elements_float, 100);
+      TestChar(capacity, sizeof(char), elements_char, 100);
+      
+}
 
-      printf("stack capacity? %lu\n",StackCapacity(stack_p));
-	 1028
 
-	for(i = 0 ; i < 10 ; i++)
+static void TestInt(size_t capacity, size_t element_size, int* elements, size_t number_of_elements)
+{	
+	stack_ty* stack_p = StackCreate(capacity, element_size);	
+	size_t i = 0;
+	int val = 0;
+
+	printf("--------- TEST %lu ---------\n",test_number);
+	printf("create\t\t");
+	if(NULL == stack_p)
 	{
-	    val = *((int*) StackPeek(stack_p));
-	    printf("val is %d\n", val);
-	    StackPop(stack_p);
-	    printf("stack size = %lu\n", StackSize(stack_p));
-	    printf("stack is empty? %d\n",StackIsEmpty(stack_p));
+		printf("failed at creating the stack with the following attributes:\n");
+		printf("capacity = %lu, element size = %lu\n", capacity, element_size);
+		return;
 	}
+	printf("PASSED\n");
+	printf("capacity\t");
+	if(capacity != StackCapacity(stack_p))
+	{
+		printf("failed: StackCreate function, in creating stack of %lu capacity\n", capacity);
+		printf("it created %lu capacity\n",StackCapacity(stack_p));
+		StackDestroy(stack_p);
+		return;
+	}
+	printf("PASSED\n");
 	
-	StackDestroy(stack_p);
-	return 0;
+		printf("push\t\t");
+		for(i = 0 ; i < number_of_elements ; i++)
+		{
+			StackPush(stack_p, &elements[i]);
+		}
+		if(StackSize(stack_p) != number_of_elements)
+		{
+			printf("size\t\t");
+			printf("failed\n");
+			return;
+		}
+		printf("PASSED\n");
+		printf("peek\t\t");
+		for(i = 0 ; i < number_of_elements ; i++)
+		{
+			val = *(int*)StackPeek(stack_p);
+			StackPop(stack_p);
+			if(val != elements[number_of_elements -1 - i])
+			{
+				printf("failed: it got %d, where it supposed to get %d\n", val, elements[number_of_elements -1 - i]);
+				return;
+			}
+		}
+
+            printf("PASSED\n");
+            printf("pop\t\t");
+            printf("PASSED\n");
+            printf("size\t\t");
+            printf("PASSED\n");
+		StackDestroy(stack_p);
+		printf("destroy\t\t");
+            printf("PASSED\n");
 	
+	++test_number;
+   
 }
 
-/*
-static void TestStackCreate()
-{
+static void TestFloat(size_t capacity, size_t element_size, float* elements, size_t number_of_elements)
+{	
+	stack_ty* stack_p =  StackCreate(capacity, element_size);	
+	size_t i = 0;
+	float val = 0;
 
-}
-static void TestStackDestroy()
-{
+
+	
+	printf("--------- TEST %lu ---------\n",test_number);
+	++test_number;
+	printf("create\t\t");
+	if(NULL == stack_p)
+	{
+		printf("failed at creating the stack with the following attributes:\n");
+		printf("capacity = %lu, element size = %lu\n", capacity, element_size);
+		return;
+	
+	}
+	printf("PASSED\n");
+	printf("capacity\t");
+	if(capacity != StackCapacity(stack_p))
+	{
+		printf("failed: StackCreate function, in creating stack of %lu capacity\n", capacity);
+		printf("it created %lu capacity\n",StackCapacity(stack_p));
+		StackDestroy(stack_p);
+		return;
+	}
+	printf("PASSED\n");
+	
+		printf("push\t\t");
+		for(i = 0 ; i < number_of_elements ; i++)
+		{
+			StackPush(stack_p, &elements[i]);
+		}
+		if(StackSize(stack_p) != number_of_elements)
+		{
+			printf("size\t\t");
+			printf("failed\n");
+			return;
+		}
+		printf("PASSED\n");
+		printf("peek\t\t");
+		for(i = 0 ; i < number_of_elements ; i++)
+		{
+			val = *(float*)StackPeek(stack_p);
+			StackPop(stack_p);
+			if(val != elements[number_of_elements -1 - i])
+			{
+				printf("failed: it got %f, where it supposed to get %f\n", val, elements[number_of_elements -1 - i]);
+				return;
+			}
+		}
+
+            printf("PASSED\n");
+            printf("pop\t\t");
+            printf("PASSED\n");
+            printf("size\t\t");
+            printf("PASSED\n");
+		StackDestroy(stack_p);
+		printf("destroy\t\t");
+            printf("PASSED\n");
 	
 
-}
-static void TestStackPush()
-{
+    
 
 }
-static void TestStackPeek()
-{
+
+static void TestChar(size_t capacity, size_t element_size, char* elements, size_t number_of_elements)
+{	
+	stack_ty* stack_p =  StackCreate(capacity, element_size);	
+	size_t i = 0;
+	char val = 0;
+
+
+	
+	printf("--------- TEST %lu ---------\n",test_number);
+	printf("create\t\t");
+	if(NULL == stack_p)
+	{
+		printf("failed at creating the stack with the following attributes:\n");
+		printf("capacity = %lu, element size = %lu\n", capacity, element_size);
+		return;
+	
+	}
+	printf("PASSED\n");
+	printf("capacity\t");
+	if(capacity != StackCapacity(stack_p))
+	{
+		printf("failed: StackCreate function, in creating stack of %lu capacity\n", capacity);
+		printf("it created %lu capacity\n",StackCapacity(stack_p));
+		StackDestroy(stack_p);
+		return;
+	}
+	printf("PASSED\n");
+	
+		printf("push\t\t");
+		for(i = 0 ; i < number_of_elements ; i++)
+		{
+			StackPush(stack_p, &elements[i]);
+		}
+		if(StackSize(stack_p) != number_of_elements)
+		{
+			printf("size\t\t");
+			printf("failed\n");
+			return;
+		}
+		printf("PASSED\n");
+		printf("peek\t\t");
+		for(i = 0 ; i < number_of_elements ; i++)
+		{
+			val = *(char*)StackPeek(stack_p);
+			StackPop(stack_p);
+			if(val != elements[number_of_elements -1 - i])
+			{
+				printf("failed: it got %c, where it supposed to get %c\n", val, elements[number_of_elements -1 - i]);
+				return;
+			}
+		}
+
+            printf("PASSED\n");
+            printf("pop\t\t");
+            printf("PASSED\n");
+            printf("size\t\t");
+            printf("PASSED\n");
+		StackDestroy(stack_p);
+		printf("destroy\t\t");
+            printf("PASSED\n");
+	
+	++test_number;
+    
 
 }
-static void TestStackPop();
-static void TestStackIsEmpty()
-{
 
-}
-static void TestStackSize()
-{
-
-}
-static void TestStackCapacity()
-{
-
-}*/
