f3ceccb13cde759d2ae93319d16dc5d564d90099
rozaline.kozly <rozaline.ko@gmail.com>
2025-12-10 10:49:46 +0200

    fix: the problem you were not multiplying with the element_size factore, which led to reading wrong data, even though it compiled, in this version you have a full working code, passed intial random pushing and poping, checking capacity, size, etc, with 1 value

--- ds/src/stack.c
+++ ds/src/stack.c
@@ -1,20 +1,32 @@
 /*--------------------------------------------------------------------------
 submitter : Rozaline Kozly
-reviewer  : 
+reviewer  : steve
 worksheet : 12 (ds - stack)
 version   : 1
 date      : 9 Dec 2025
 stage     : pre-review
 ----------------------------------------------------------------------------*/
-#include "../include/stack.h"
 #include <stdlib.h>			/* malloc(), free() */
-#include <string.h>			/* memmove() */
+#include <string.h>			/* memcpy() */
+#include <assert.h>			/* assert() */
+#include "../include/stack.h"
+
+/* shortcuts, used for created stacks pointers of name "stack_p" */
+#define CURRENT_EMPTY_PLACEMENT_IN_STK		      ((stack_p -> stack) + (stack_p -> stack_size * stack_p -> element_size))
+#define NUMBER_OF_ELEMENTS_IN_STK			      (stack_p -> stack_size )
+#define FULL_CAPACITY_STK					(stack_p -> stack_size == stack_p -> capacity)
+#define CURRENT_ELEMENT_IN_STK				(stack_p -> stack + (stack_p-> stack_size - 1) * stack_p -> element_size)
+
 struct stack_s
 {
-	size_t capacity;
+	/*holds maximum number of elements the stack can hold*/
+	size_t capacity;	
+	/*holds size per element, all elements share same size*/
 	size_t element_size;
-	size_t stk_size;
-	char stk[1];		/* flexible array c89 */
+	/*holds number of elements within the stack at each point in time*/
+	size_t stack_size;
+      /*flexible array c89, this is the stack! */
+	char stack[1];		
 };
 
 typedef enum
@@ -23,49 +35,73 @@ typedef enum
 	TRUE = 1
 }bool_ty;
 
+
 stack_ty* StackCreate(size_t capacity, size_t element_size)
 {
-	stack_ty* stk_p = NULL;
+	stack_ty* stack_p = NULL;
 	
-	stk_p = (stack_ty*)malloc(sizeof(stack_ty) + capacity * element_size);
-	stk_p -> capacity = capacity;
-	stk_p -> element_size = element_size;
-	stk_p -> stk_size = 0;
+	stack_p = (stack_ty*)malloc(sizeof(stack_ty) + (capacity * element_size));
+	if(NULL == stack_p)
+	{
+		return NULL;
+	}
+	stack_p -> capacity = capacity;
+	stack_p -> element_size = element_size;
+	NUMBER_OF_ELEMENTS_IN_STK = 0;
 	
-	return stk_p;
+	return stack_p;
 }
 
 void StackDestroy(stack_ty* stack_p)
 {
-	if(NULL != stack_p)
-	{
+	size_t i = 0;
+	
+      if(NULL != stack_p)
+	{	/*remove all stack elements*/
+	      for(i = 0 ; i < NUMBER_OF_ELEMENTS_IN_STK  ; i++)
+	      {
+	      	StackPop(stack_p);
+	      }
      		free(stack_p);
+     		/*prevent dangling pointer*/
      		stack_p = NULL;
 	}
 }
 
 void StackPush(stack_ty* stack_p, void* new_element)
 {
-	 /*stack_p -> stk[stack_p -> stk_size] = new_element;
-	 ++(stack_p -> stk_size);*/
-	memmove(stack_p -> stk + stack_p -> stk_size, new_element, stack_p -> element_size);
-	++stack_p->stk_size;
+	if(FULL_CAPACITY_STK)
+	{
+		/* do nothing */
+		return; 
+	}
+	memcpy(CURRENT_EMPTY_PLACEMENT_IN_STK, new_element, stack_p -> element_size);
+	++NUMBER_OF_ELEMENTS_IN_STK;
 }
 
 void* StackPeek(const stack_ty* stack_p)
 {
-      return (void*)(stack_p-> stk + (stack_p->stk_size - 1));
-	/*return (void*)(stack_p -> stk[(stack_p -> stk_size) - 1]);*/
+	if(0 == NUMBER_OF_ELEMENTS_IN_STK)
+	{
+		/* nothing to read*/
+		return NULL;
+	}
+      return (void*)(CURRENT_ELEMENT_IN_STK);
 }
 
 void StackPop(stack_ty* stack_p)
 {
-	--(stack_p -> stk_size);
+	if(0 == NUMBER_OF_ELEMENTS_IN_STK)
+	{
+		/* do nothing */
+		return;
+	}
+	--(NUMBER_OF_ELEMENTS_IN_STK);
 }
 
 size_t StackSize(const stack_ty* stack_p)
 {
-	return (stack_p -> stk_size);
+	return (NUMBER_OF_ELEMENTS_IN_STK);
 }
 
 int StackIsEmpty(const stack_ty* stack_p)
