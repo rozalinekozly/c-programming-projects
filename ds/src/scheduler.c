7a99cb066d5c158041d51692d95547aa0c8e826e
rozaline.kozly <rozaline.ko@gmail.com>
2026-01-07 16:47:17 +0200

    add: scheduler managing strucutre

--- ds/src/scheduler.c
+++ ds/src/scheduler.c
@@ -4,3 +4,21 @@ reviewer : ??
 version : 1
 date : 7 Jan 2026
 ------------------------------------------------------------------------------*/
+#include <pqueue.h>
+#include <uid.h>
+#include <task.h>
+
+typedef enum
+{
+	NONE = 0,
+	RMV = 1,
+	CLR = 2
+}special_op_ty;
+
+struct scheduler
+{
+	pq_ty* pq;
+	uid_ty curr_uid;
+	special_op_ty sp_op;  /* none, remove, clear*/
+};
+
