144c8249a62d538402eb495dad79ba8eb0d58b62
rozaline.kozly <rozaline.ko@gmail.com>
2026-01-28 16:28:22 +0200

    add: (src,header,test) for merge sort

--- ds/src/sorts.c
+++ ds/src/sorts.c
@@ -4,12 +4,15 @@ submitter: rozaline
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 /*----------------------------------------------------------------------------*/
 #include "sorts.h"		/*API*/
 /*----------------------------------------------------------------------------*/
 #define NOT_FOUND		-1
 /*----------------------------------------------------------------------------*/
 static int BinarySearchRecursiveWrapped(int arr[], int left, int right , int key);
+static void MergeSortWrapper(int* arr, int* temp, size_t left, size_t right);
+static void Merge(int* arr, int* temp, size_t left, size_t mid, size_t right);
 /*----------------------------------------------------------------------------*/
 int BinarySearchIterative(int arr[], size_t arr_size, int key)
 {
@@ -78,5 +81,79 @@ static int BinarySearchRecursiveWrapped(int arr[], int left, int right , int key
 	{
 		return BinarySearchRecursiveWrapped(arr, left, mid - 1, key);  
 	}
+}
+/*----------------------------------------------------------------------------*/
+int MergeSort(int* arr_to_sort, size_t num_elements)
+{
+    int* temp = NULL;
+    
+    assert(arr_to_sort != NULL);
+    
+    if (num_elements <= 1)
+    {
+        return 0;
+    }
+    
+    temp = (int*)malloc(num_elements * sizeof(int));
+    if (NULL == temp)
+    {
+        return -1;
+    }
+    
+    MergeSortWrapper(arr_to_sort, temp, 0, num_elements - 1);
+    
+    free(temp);
+    
+    return 0;
+}
+/*----------------------------------------------------------------------------*/
+static void MergeSortWrapper(int* arr, int* temp, size_t left, size_t right)
+{
+    size_t mid = 0;
+    
+    if (left >= right)
+    {
+        return;
+    }
+    
+    mid = left + (right - left) / 2;
+    
+    MergeSortWrapper(arr, temp, left, mid);
+    MergeSortWrapper(arr, temp, mid + 1, right);
+    Merge(arr, temp, left, mid, right);
+}
+/*----------------------------------------------------------------------------*/
+static void Merge(int* arr, int* temp, size_t left, size_t mid, size_t right)
+{
+    size_t i = left;
+    size_t j = mid + 1;
+    size_t k = left;
+    
+    while (i <= mid && j <= right)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k++] = arr[i++];
+        }
+        else
+        {
+            temp[k++] = arr[j++];
+        }
+    }
+    
+    while (i <= mid)
+    {
+        temp[k++] = arr[i++];
+    }
+    
+    while (j <= right)
+    {
+        temp[k++] = arr[j++];
+    }
+    
+    for (i = left; i <= right; i++)
+    {
+        arr[i] = temp[i];
+    }
 }
 
