9ac2ff769113fa2e8d5b65f44290169cc5ff6f1f
rozaline.kozly <rozaline.ko@gmail.com>
2026-02-17 09:15:35 +0200

    feat: add utils.h with DEBUG_ONLY and BAD_MEM macros
    Introduces a utility header providing macros for conditional debug code
    execution. DEBUG_ONLY wraps expressions that only run in debug mode,
    BAD_MEM sets freed pointers to 0xDEADBEEF to catch use-after-free bugs.

---
+++ ds/include/utils.h
@@ -0,0 +1,16 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <stddef.h> /* size_t */
+
+/* Casts a pointer to a bad memory address for debugging */
+#define BAD_MEM(type) ((type)0xDEADBEEF)
+
+/* Executes code only in debug mode (when NDEBUG is not defined) */
+#ifdef NDEBUG
+    #define DEBUG_ONLY(x) ((void)0)
+#else
+    #define DEBUG_ONLY(x) (x)
+#endif
+
+#endif /* __UTILS_H__ */
