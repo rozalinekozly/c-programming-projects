4e50c342e23425bda8b91a84fbe8a12f4d413698
rozaline.kozly <rozaline.ko@gmail.com>
2026-02-17 12:28:59 +0200

    add psudo: divided main unit (heapsort func) into 2 main components: build heap from array and build sorted array from heap. also added forward declarations on aux funcs

--- ds/src/heap_sort.c
+++ ds/src/heap_sort.c
@@ -1,14 +1,29 @@
+#include <stddef.h>     /* size_t */
+
 #include "heap_sort.h"
+/*--------------------------forward declarations------------------------------*/
+static BuildHeapIMP(int* arr_, size_t arr_size_);
+static BuildSortedArrayFromHeapIMP(int* arr, size_t arr_size_);
 /*----------------------------------------------------------------------------*/
 void HeapSort(int* arr_, size_t arr_size_)
 {
 	/*asserts*/
+	/*construct a valid heap by calling BuildHeapIMP() */
+	/*construct a sorted array by calling BuildSortedArrayFromHeapIMP()*/
+}
+
+static BuildHeapIMP(int* arr_, size_t arr_size_)
+{
+	/*asserts*/
 	/*iterate on array*/
 		/*heapify up */
-		/*now you have a valid binary heap*/
+}
+
+static BuildSortedArrayFromHeapIMP(int* arr, size_t arr_size_)
+{
+	/*asserts*/
 	/*iterate on array*/
 		/*swap root with rightmost leaf*/
 		/*decrement heap size by 1*/
 		/*heapify down "new root"*/
-	/*now you have a sorted array*/
 }
