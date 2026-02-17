f071b05a3c23504c93d718dc9c61773b8801ab97
rozaline.kozly <rozaline.ko@gmail.com>
2026-02-17 12:19:20 +0200

    add psudo: outline implementation plan

---
+++ ds/src/heap_sort.c
@@ -0,0 +1,14 @@
+#include "heap_sort.h"
+/*----------------------------------------------------------------------------*/
+void HeapSort(int* arr_, size_t arr_size_)
+{
+	/*asserts*/
+	/*iterate on array*/
+		/*heapify up */
+		/*now you have a valid binary heap*/
+	/*iterate on array*/
+		/*swap root with rightmost leaf*/
+		/*decrement heap size by 1*/
+		/*heapify down "new root"*/
+	/*now you have a sorted array*/
+}
