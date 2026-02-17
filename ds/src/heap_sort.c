48cf5d7e9e4ab47fdfce4dbdb7793aa162d3c8b1
rozaline.kozly <rozaline.ko@gmail.com>
2026-02-17 12:44:01 +0200

    add psudo: coordinations mapping via Start function, adapted to heap of ints

--- ds/src/heap_sort.c
+++ ds/src/heap_sort.c
@@ -4,6 +4,7 @@
 /*--------------------------forward declarations------------------------------*/
 static BuildHeapIMP(int* arr_, size_t arr_size_);
 static BuildSortedArrayFromHeapIMP(int* arr, size_t arr_size_);
+static int* GetStartIMP(int* arr);
 /*----------------------------------------------------------------------------*/
 void HeapSort(int* arr_, size_t arr_size_)
 {
@@ -11,19 +12,29 @@ void HeapSort(int* arr_, size_t arr_size_)
 	/*construct a valid heap by calling BuildHeapIMP() */
 	/*construct a sorted array by calling BuildSortedArrayFromHeapIMP()*/
 }
-
+/*----------------------------------------------------------------------------*/
 static BuildHeapIMP(int* arr_, size_t arr_size_)
 {
 	/*asserts*/
+	/* get start pointer */
 	/*iterate on array*/
 		/*heapify up */
 }
-
+/*----------------------------------------------------------------------------*/
 static BuildSortedArrayFromHeapIMP(int* arr, size_t arr_size_)
 {
 	/*asserts*/
+	/* get start pointer */
 	/*iterate on array*/
 		/*swap root with rightmost leaf*/
 		/*decrement heap size by 1*/
 		/*heapify down "new root"*/
+}
+/*----------------------------------------------------------------------------*/
+static int* GetStartIMP(int* arr)
+{
+    /* assert valid arr */
+    /* get pointer to first element (index 0) */
+    /* subtract one pointer size to shift back */
+    /* return shifted pointer */
 }
