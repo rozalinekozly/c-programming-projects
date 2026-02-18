b140177e0c9407f3701ca0d0427f5f410f9dd3da
rozaline.kozly <rozaline.ko@gmail.com>
2026-02-18 09:38:34 +0200

    style: updatre the names to follownaming conventions

--- ds/src/heap_sort.c
+++ ds/src/heap_sort.c
@@ -4,10 +4,10 @@
 /*--------------------------forward declarations------------------------------*/
 static void BuildHeapIMP(int* arr_, size_t arr_size_);
 static void BuildSortedArrayFromHeapIMP(int* arr_, size_t arr_size_);
-static int* GetStartIMP(int* arr);
+static int* GetStartIMP(int* arr_);
 static void HeapifyUpIMP(int* arr_, size_t idx_);
 static void HeapifyDownIMP(int* arr_, size_t heap_size_);
-static void SwapIMP(int* a, int* b);
+static void SwapIMP(int* a_, int* b_);
 static size_t GetParentIMP(size_t idx_);
 static size_t RChildIMP(size_t idx_);
 static size_t LChildIMP(size_t idx_);
@@ -71,7 +71,7 @@ static void HeapifyDownIMP(int* arr_, size_t heap_size_)
             /* stop */
 }
 /*----------------------------------------------------------------------------*/
-static void SwapIMP(int* a, int* b)
+static void SwapIMP(int* a_, int* b_)
 {
 
     /* use temp variable to swap values */
