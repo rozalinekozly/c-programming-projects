7648c91b14774627da3cd88902d70e72daf13500
rozaline.kozly <rozaline.ko@gmail.com>
2026-02-18 09:37:50 +0200

    rename: SwapIMP parameters from idx1_/ids2_ to a/b

--- ds/src/heap_sort.c
+++ ds/src/heap_sort.c
@@ -7,7 +7,7 @@ static void BuildSortedArrayFromHeapIMP(int* arr_, size_t arr_size_);
 static int* GetStartIMP(int* arr);
 static void HeapifyUpIMP(int* arr_, size_t idx_);
 static void HeapifyDownIMP(int* arr_, size_t heap_size_);
-static void SwapIMP(int* idx1_, int* idx2_);
+static void SwapIMP(int* a, int* b);
 static size_t GetParentIMP(size_t idx_);
 static size_t RChildIMP(size_t idx_);
 static size_t LChildIMP(size_t idx_);
@@ -71,7 +71,7 @@ static void HeapifyDownIMP(int* arr_, size_t heap_size_)
             /* stop */
 }
 /*----------------------------------------------------------------------------*/
-static void SwapIMP(int* idx1_, int* idx2_)
+static void SwapIMP(int* a, int* b)
 {
 
     /* use temp variable to swap values */
