61392f85774610a14b27c1b5945acaea2b5f6c97
rozaline.kozly <rozaline.ko@gmail.com>
2026-02-23 01:05:42 +0200

    add: allocate unit test

--- ds/test/dhcp_test.c
+++ ds/test/dhcp_test.c
@@ -17,8 +17,39 @@ void TestCreateDestroy()
     printf("PASS: DhcpDestroy succeeded\n");
 }
 
+void TestAllocateIp()
+{
+    dhcp_ty* dhcp = DhcpCreate(0, 8);
+    addr_ty ip = 0;
+
+    /* allocate specific address */
+    ip = DhcpAllocateIp(dhcp, 5);
+    if (5 == ip)
+    {
+        printf("PASS: allocated requested address\n");
+    }
+    else
+    {
+        printf("FAIL: expected 5, got %lu\n", ip);
+    }
+
+    /* allocate same address again - should get next available */
+    ip = DhcpAllocateIp(dhcp, 5);
+    if (5 != ip && 0 != ip)
+    {
+        printf("PASS: got next available address\n");
+    }
+    else
+    {
+        printf("FAIL: expected next available, got %lu\n", ip);
+    }
+
+    DhcpDestroy(dhcp);
+}
+
 int main()
 {
     TestCreateDestroy();
+    TestAllocateIp();
     return 0;
 }
