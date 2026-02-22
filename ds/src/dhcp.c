fb239e335ea280d526ee11c9efc84525fa0186a9
rozaline.kozly <rozaline.ko@gmail.com>
2026-02-23 00:33:41 +0200

    add: psudo code for create function - round 1

--- ds/src/dhcp.c
+++ ds/src/dhcp.c
@@ -4,3 +4,21 @@ struct dhcp
     addr_ty subnet_id;    /* the network part of the address (the common part)*/
     addr_ty subnet_mask;  /* separates network bits from host bits*/
 };
+
+dhcp_ty* DhcpCreate(addr_ty subnet_id_, unsigned int host_numb_)
+{
+	/* asserts */
+	/* allocate dhcp */
+	/* if failed return NULL */
+	/* calculate host_numb (32 - subnet bits) */
+	/* allocate btrie using BTrieCreate(host_numb) */
+	/* if failed free dhcp and return NULL */
+	/* set fields (trie, subnet_id, subnet_mask) */
+	/* reserve server address 111...110 using BTrieGet */
+	/* reserve broadcast address 111...111 using BTrieGet */
+	/* if any reservation failed destroy trie, free dhcp and return NULL */
+	/* return dhcp */
+}
+
+
+
