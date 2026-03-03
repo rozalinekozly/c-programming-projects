db0793aa993fa1e12e59221b587112997a4b69b8
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-03 02:54:13 +0200

    simple shell: adding round 2 psudo code, refined on handling internal command, by adding 2 helper functions to detect if it's internal and to run that internal, in addition added an enum to give a code for each internal command

--- system_programming/src/shell.c
+++ system_programming/src/shell.c
@@ -1,10 +1,28 @@
+typedef enum
+{
+	NOT_INTERNAL_CMD = 0,
+	EXIT = 1
+}internal_cmd_ty;
+
+/* return value : 0 = not an internal command
+				  other value = relevant internal command number*/
+int IsInternalCmd(command);
+
+void RunInternal(internal_cmd);
+
 int main()
 {
+	int internal_cmd = NOT_INTERNAL_CMD;
     /* while 1 */
         /* print prefix <username>@<machine_name>:<current_dir>$ */
         /* read input and split it into words */
         /* if input is an internal command*/
+        internal_cmd = IsInternalCmd(command);
+        if(internal_cmd)
+        {
         	/*call relevant function*/
+        	RunInternal(internal_cmd);
+        }
         /*else */
 			/* hand the command to the OS to run and save return status*/
 			/*if running command failed*/
@@ -12,3 +30,17 @@ int main()
     }
     return 0;
 }
+
+int IsInternalCmd(command)
+{
+	/*if command is one of internal_cmd_ty*/
+		/*return command's code*/
+	/*else*/
+		/*return NOT_INTERNAL_CMD */
+}
+
+void RunInternal(internal_cmd)
+{
+	/*run internal command according to the passed code*/
+}
+
