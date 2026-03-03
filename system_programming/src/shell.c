7e4f144152dcf8cf4af4f3a4ec811945bfe5deae
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-03 02:27:33 +0200

    simple shell: adding round 1 psudo code

---
+++ system_programming/src/shell.c
@@ -0,0 +1,14 @@
+int main()
+{
+    /* while 1 */
+        /* print prefix <username>@<machine_name>:<current_dir>$ */
+        /* read input and split it into words */
+        /* if input is an internal command*/
+        	/*call relevant function*/
+        /*else */
+			/* hand the command to the OS to run and save return status*/
+			/*if running command failed*/
+				/*print to stderr proper error massage*/
+    }
+    return 0;
+}
