f73353fea21a03a6e05b0e7e150b70064fa18fc2
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-03 14:19:21 +0200

    ping pong sigs: adding round 1 psudo-code of exe 1

---
+++ system_programming/src/pingpong.c
@@ -0,0 +1,11 @@
+#include <unistd.h>		/*fork(), pid_t*/
+
+int main() {
+    /*fork and save pid*/
+    /*if fork failed*/
+    	/*handle failure*/
+    /* if child*/
+    	/*call child_process*/
+    /*if parent*/
+    	/*call parent_process */
+}
