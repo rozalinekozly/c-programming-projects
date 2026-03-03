05e2495e5091bf1e7c45a7adb2b704981090c61d
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-03 03:16:48 +0200

    simple shell: writing implementation of print prefix function, added relevant includes (libraries in c)

--- system_programming/src/shell.c
+++ system_programming/src/shell.c
@@ -1,3 +1,8 @@
+#include <limits.h>	   /*macros: HOST_NAME_MAX, PATH_MAX*/
+#include <stdio.h>	   /*printf()*/
+#include <unistd.h>    /*getuid(), gethostname(), getcwd()*/
+#include <pwd.h>  	   /*getpwuid()*/
+
 typedef enum
 {
 	NOT_INTERNAL_CMD = 0,
@@ -10,11 +15,15 @@ static int IsInternalCmdIMP(command_);
 
 static void RunInternalIMP(internal_cmd_);
 
+static void PrintPrefixIMP(void);
 int main()
 {
 	int internal_cmd = NOT_INTERNAL_CMD;
     /* while 1 */
-        /* print prefix <username>@<machine_name>:<current_dir>$ */
+    while(1)
+    {
+    	 /* print prefix <username>@<machine_name>:<current_dir>$ */
+    	 PrintPrefixIMP();
         /* read input and split it into words */
         /* if input is an internal command*/
         internal_cmd = IsInternalCmdIMP(command);
@@ -29,6 +38,22 @@ int main()
 				/*print to stderr proper error massage*/
     }
     return 0;
+}
+
+static void PrintPrefixIMP()
+{
+	/*declare prefix's paths'*/
+	char machine_name[HOST_NAME_MAX] = {0};
+    char curr_dir[PATH_MAX] = {0};
+    char* username = NULL;
+	
+	/*extract them using proper sys calls*/
+    username = getpwuid(getuid())->pw_name;
+    gethostname(machine_name, sizeof(machine_name));
+    getcwd(curr_dir, sizeof(curr_dir));
+    
+    /*print prefix with the order username@machine name:current_dir$*/
+    printf("%s@%s:%s$ ", username, machine_name, curr_dir);
 }
 
 static int IsInternalCmdIMP(command_)
