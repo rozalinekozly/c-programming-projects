85f8309b79382e0f178c491b0a56bd55f2802d3c
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-09 12:38:17 +0200

    100k threads: compiled and executable version, but fails at index 1 for some reason (too early)

--- system_programming/src/100k_threads.c
+++ system_programming/src/100k_threads.c
@@ -2,11 +2,40 @@
 #include <pthread.h>		/*pthread_create*/
 #include <stdio.h>			/*printf()*/
 
+enum
+{
+	SIZE = 500000
+};
+
+int arr[SIZE] = {0};
+
 void* AssignIndexIMP(void* idx_);
 
 int main()
 {
-	int arr[500000] = {0};
+	pthread_t threads[SIZE];
+	int i = 0;
+
+	for(i = 0 ; i < SIZE ; i++)
+	{
+		pthread_create(&threads[i], NULL, AssignIndexIMP, (void*)&i);
+	}
+	
+	for(i = 0 ; i < SIZE ; i++)
+	{
+		if(i != arr[i])
+		{
+			printf("detected first failure at index %d\n", i);
+			return 1;
+		}
+		else
+		{
+			printf("success %d\n", i);
+		}
+
+	}
+	
+	return 0;
 }
  /*int pthread_create(pthread_t *restrict thread,
                           const pthread_attr_t *restrict attr,
@@ -17,7 +46,7 @@ int main()
 
 void* AssignIndexIMP(void* idx_)
 {
-	int idx = (int)idx_;
+	int idx = *(int*)idx_;
 	
 	arr[idx] = (int)idx;
 	return NULL;
