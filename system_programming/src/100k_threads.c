c82e52afabdccac11641f75ec1eeeec9364da541
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-09 12:24:35 +0200

    100k threads: defined routine for threads, same prototype as defined as an arg in pthread_create function

--- system_programming/src/100k_threads.c
+++ system_programming/src/100k_threads.c
@@ -1,3 +1,24 @@
 /* 100k thread */
-#include <pthread.h>
+#include <pthread.h>		/*pthread_create*/
+#include <stdio.h>			/*printf()*/
 
+void* AssignIndexIMP(void* idx_);
+
+int main()
+{
+	int arr[500000] = {0};
+}
+ /*int pthread_create(pthread_t *restrict thread,
+                          const pthread_attr_t *restrict attr,
+                          void *(*start_routine)(void *),
+                          void *restrict arg);*/
+
+
+
+void* AssignIndexIMP(void* idx_)
+{
+	size_t idx = (size_t)idx_;
+	
+	arr[idx] = (int)idx;
+	return NULL;
+}
