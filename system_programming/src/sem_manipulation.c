3806cf21f1999fc1615d5b9571a508dcd3aeed2d
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-18 16:07:29 +0200

    sem manipulation: adding first round of psudo code, main function contains all the logic

---
+++ system_programming/src/sem_manipulation.c
@@ -0,0 +1,57 @@
+int main(int argc, char* argv[])
+{
+	/*validate argc value is 2*/
+		/*if not exit*/
+	
+	/*construct a key*/
+	/*create file with arv[1] if not exist open*/
+	/*get key file is argv[1]*/
+	
+	/*create/connect to semaphore*/
+	/*if failed*/
+		/*if with errno EEXIST*/
+			/*connect process to semaphore*/
+		/*otherwise*/
+			/*exit, and print failure or whatever*/
+	/*else (then it's the creator)*/
+		/*initialize semaphore and set it's value to somevalue*/
+		
+	/*loop while 1*/
+		/*read whole line with fgets*/
+		/*parse with sscanf, cmd, number, undo_str*/
+		/*check how many tokens returned store it in tokens_count var*/
+		
+		/*switch cmd*/
+		    /*case v*/
+		        /*print value*/
+		    
+		    /*case x*/
+		        /*cleanup*/
+		        /*exit*/
+		    
+		    /*case D*/
+		    	/*if tokens = 3*/
+		    		/*vlaidate input correctness (undo_str = undo)*/
+		    		/*if wrong*/
+		    			/*print messgae to user*/
+		    			/*continue*/
+		    		/*else*/
+		    			/*set sem_flg to be SEM_UNDO*/
+				/*else*/
+					/*set sem_flg = 0 (no undo)*/
+		        /*set sem_op to be -number*/
+		        /*call semop with op*/
+		    
+		    /*case I*/
+		    	/*if tokens = 3*/
+		    		/*vlaidate input correctness (undo_str = undo)*/
+		    		/*if wrong*/
+		    			/*print messgae to user*/
+		    			/*continue*/
+		    		/*else*/
+		    			/*set sem_flg to be SEM_UNDO*/
+				/*else*/
+					/*set sem_flg = 0 (no undo)*/
+		        /*set sem_op to be number*/
+		        /*call semop with op*/
+}
