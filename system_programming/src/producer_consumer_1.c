3c34e0780e5b9f52778f15ac04878d5686f488f4
rozaline.kozly <rozaline.ko@gmail.com>
2026-03-23 14:17:40 +0200

    system_programming producer_consumer_1.c: adding and fixing skeleton to fit with exercise requirements, made sections in code more cleaner, such as, forward declarations and order of funcitons, in addition added main function implementation, creates 2 threads to their relevant routines and then joins them

--- system_programming/src/producer_consumer_1.c
+++ system_programming/src/producer_consumer_1.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <pthread.h>	/**/
 
+#include "utils.h"
+/*----------------------------------------------------------------------------*/
 /*lock status*/
 enum
 {
@@ -12,10 +14,39 @@ enum
 
 typedef int spinlock_ty;
 
+/*declare on a lock*/
 spinlock_ty g_is_locked = NOT_LOCKED;
-
+/*declare on a "shared" memory*/
+/*producer : increments shared_resource
+ cosumer reads and print the value*/
 int shared_resource = 0;
+/*-------------------------- forward declarations ----------------------------*/
+static void* ProducerThreadIMP(void* arg);
+static void* ConsumerThreadIMP(void* arg);
 
+static int ProduceIMP(void);
+static void ConsumeIMP(int product);
+
+static void SpinLockIMP(void);
+static void SpinUnlockIMP(void);
+/*----------------------------------------------------------------------------*/
+int main()
+{
+    pthread_t producer;
+    pthread_t consumer;
+
+	/*create a producer thread, to perform ProducerThreadIMP()*/
+    while(0 != pthread_create(&producer, NULL, ProducerThreadIMP, NULL));
+    /*create a consumer thread, to perform ConsumerThreadIMP()*/
+    while(0 != pthread_create(&consumer, NULL, ConsumerThreadIMP, NULL));
+	
+	/*join threads before exiting*/
+    EXIT_IF_BAD(0 == pthread_join(producer, NULL), 1, "failed to join producer");
+    EXIT_IF_BAD(0 == pthread_join(consumer, NULL), 1, "failed to join consumer");
+
+    return 0;
+}
+/*----------------------------------------------------------------------------*/
 /*acquire lock*/
 void SpinLock()
 {
@@ -24,13 +55,34 @@ void SpinLock()
     	/*do nothing (busy wait))*/
     }
 }
-
+/*----------------------------------------------------------------------------*/
 /*release lock*/
 void SpinUnlock()
 {
     __atomic_store_n(&g_is_locked, NOT_LOCKED, __ATOMIC_RELEASE);
 }
+/*----------------------------------------------------------------------------*/
+static void* ProducerThreadIMP(void* arg)
+{
+	UNUSED(arg);
+}
+/*----------------------------------------------------------------------------*/
+static void* ConsumerThreadIMP(void* arg)
+{
+	UNUSED(arg);
+}
+/*----------------------------------------------------------------------------*/
+static int ProduceIMP(void)
+{
 
+}
+/*----------------------------------------------------------------------------*/
+static void ConsumeIMP(int product)
+{
+
+}
+/*----------------------------------------------------------------------------*/
+/*rmv later*/
 void* Routine(void* arg)
 {
     int i;
@@ -47,17 +99,4 @@ void* Routine(void* arg)
     return NULL;
 }
 
-int main()
-{
-    pthread_t t1, t2;
 
-    pthread_create(&t1, NULL, Routine, NULL);
-    pthread_create(&t2, NULL, Routine, NULL);
-
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-
-    printf("Final value: %d\n", shared_resource);
-
-    return 0;
-}
